be97bc8b7d9b48c7cfd72aaa38f91180df9d7a56
vitalii <vitalii.cherukha@gmail.com>
2025-07-18 17:09:23 +0300

    add hw

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,13 @@
+import axios from 'axios';
+
+export const getImagesByQuery = query => {
+  return axios.get(`https://pixabay.com/api/`, {
+    params: {
+      key: '51362773-b845efdff4000eb7d694ec90c',
+      q: query,
+      orientation: 'horizontal',
+      safesearch: 'true',
+      image_type: 'photo',
+    },
+  });
+};
---
+++ src/main.js
@@ -0,0 +1,42 @@
+import SimpleLightbox from 'simplelightbox';
+import iziToast from 'izitoast';
+import axios, { isCancel, AxiosError } from 'axios';
+import { getImagesByQuery } from './js/pixabay-api';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions';
+
+const refs = {
+  searchForm: document.querySelector('.js-search-form'),
+  galleryList: document.querySelector('.js-gallery'),
+  loader: document.querySelector('.loader'),
+};
+
+const onSearchFormSubmit = e => {
+  e.preventDefault();
+  clearGallery();
+  showLoader();
+  const searchValue = e.target.elements['search-text'].value.trim();
+  getImagesByQuery(searchValue)
+    .then(({ data }) => {
+      if (data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+      }
+      createGallery(data.hits);
+    })
+    .catch(err => {
+      console.log(err);
+    })
+    .finally(() => {
+      hideLoader();
+    });
+};
+
+refs.searchForm.addEventListener('submit', onSearchFormSubmit);
