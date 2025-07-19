cb0e6d8b931229f9cf4c53c7e885ffae632ffdc7
vitalii <vitalii.cherukha@gmail.com>
2025-07-19 09:12:24 +0300

    add hw

--- src/main.js
+++ src/main.js
@@ -20,16 +20,26 @@ const onSearchFormSubmit = e => {
   clearGallery();
   showLoader();
   const searchValue = e.target.elements['search-text'].value.trim();
+  if (searchValue === '') {
+    iziToast.error({
+      message:
+        'Sorry, there are no images matching your search query. Please try again!',
+      position: 'topRight',
+    });
+    searchValue;
+    return;
+  }
   getImagesByQuery(searchValue)
-    .then(({ data }) => {
-      if (data.hits.length === 0) {
+    .then(({ hits }) => {
+      if (hits.length === 0) {
         iziToast.error({
           message:
             'Sorry, there are no images matching your search query. Please try again!',
           position: 'topRight',
         });
+        return;
       }
-      createGallery(data.hits);
+      createGallery(hits);
     })
     .catch(err => {
       console.log(err);
